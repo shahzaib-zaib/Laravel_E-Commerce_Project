b5ac2ea459cf2361257c0aec7f328296360c380e
Shahzaib <shezy.zaib@gmail.com>
2022-03-21 04:41:23 +0500

    Updated Admin Credentials

--- resources/views/livewire/admin/admin-order-component.blade.php
+++ resources/views/livewire/admin/admin-order-component.blade.php
@@ -1,4 +1,12 @@
 <div>
+    <style>
+        nav svg{
+            height: 20px;
+        }
+        nav .hidden{
+            display: block !important;
+        }
+    </style>
     <div class="container" style="padding: 30px 0;">
         <div class="row">
             <div class="col-md-12">
@@ -27,18 +35,18 @@
                             <tbody>
                                 @foreach ($orders as $order)
                                 <tr>
-                                    <td>{{ $order->id }}</td>
-                                    <td>{{ $order->subtotal }}</td>
-                                    <td>{{ $order->discount }}</td>
-                                    <td>{{ $order->tax }}</td>
-                                    <td>{{ $order->total }}</td>
-                                    <td>{{ $order->firstname }}</td>
-                                    <td>{{ $order->lastname }}</td>
-                                    <td>{{ $order->mobile }}</td>
-                                    <td>{{ $order->email }}</td>
-                                    <td>{{ $order->zipcode }}</td>
-                                    <td>{{ $order->status }}</td>
-                                    <td>{{ $order->created_at }}</td>
+                                    <td>{{$order->id}}</td>
+                                    <td>${{$order->subtotal}}</td>
+                                    <td>${{$order->discount}}</td>
+                                    <td>${{$order->tax}}</td>
+                                    <td>${{$order->total}}</td>
+                                    <td>{{$order->firstname}}</td>
+                                    <td>{{$order->lastname}}</td>
+                                    <td>{{$order->mobile}}</td>
+                                    <td>{{$order->email}}</td>
+                                    <td>{{$order->zipcode}}</td>
+                                    <td>{{$order->status}}</td>
+                                    <td>{{$order->created_at}}</td>
                                 </tr>
                                 @endforeach
                             </tbody>
