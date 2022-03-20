9aaa4ab733867f6372bae4e2ad6d9209b744a85a
Shahzaib <shezy.zaib@gmail.com>
2022-03-20 12:13:03 +0500

    Updated Project

--- resources/views/livewire/checkout-component.blade.php
+++ resources/views/livewire/checkout-component.blade.php
@@ -18,22 +18,27 @@
 									<p class="row-in-form">
 										<label for="fname">first name<span>*</span></label>
 										<input type="text" name="fname" value="" placeholder="Your name" wire:model="firstname">
+										@error('firstname') <span class="text-danger">{{ $message }}</span> @enderror
 									</p>
 									<p class="row-in-form">
 										<label for="lname">last name<span>*</span></label>
 										<input type="text" name="lname" value="" placeholder="Your last name" wire:model="lastname">
+										@error('lastname') <span class="text-danger">{{ $message }}</span> @enderror
 									</p>
 									<p class="row-in-form">
 										<label for="email">Email Addreess:</label>
 										<input type="email" name="email" value="" placeholder="Type your email" wire:model="email">
+										@error('email') <span class="text-danger">{{ $message }}</span> @enderror
 									</p>
 									<p class="row-in-form">
 										<label for="phone">Phone number<span>*</span></label>
 										<input type="number" name="phone" value="" placeholder="10 digits format" wire:model="mobile">
+										@error('mobile') <span class="text-danger">{{ $message }}</span> @enderror
 									</p>
 									<p class="row-in-form">
 										<label for="add">Line1:</label>
 										<input type="text" name="add" value="" placeholder="Street at apartment number" wire:model="line1">
+										@error('line1') <span class="text-danger">{{ $message }}</span> @enderror
 									</p>
 									<p class="row-in-form">
 										<label for="add">Line2:</label>
@@ -42,18 +47,22 @@
 									<p class="row-in-form">
 										<label for="country">Country<span>*</span></label>
 										<input type="text" name="country" value="" placeholder="United States" wire:model="country">
+										@error('country') <span class="text-danger">{{ $message }}</span> @enderror
 									</p>
 									<p class="row-in-form">
 										<label for="city">Province<span>*</span></label>
 										<input type="text" name="province" value="" placeholder="Province" wire:model="province">
+										@error('province') <span class="text-danger">{{ $message }}</span> @enderror
 									</p>
 									<p class="row-in-form">
 										<label for="city">Town / City<span>*</span></label>
 										<input type="text" name="city" value="" placeholder="City name" wire:model="city">
+										@error('city') <span class="text-danger">{{ $message }}</span> @enderror
 									</p>
 									<p class="row-in-form">
 										<label for="zip-code">Postcode / ZIP:</label>
 										<input type="number" name="zip-code" value="" placeholder="Your postal code" wire:model="zipcode">
+										@error('zipcode') <span class="text-danger">{{ $message }}</span> @enderror
 									</p>
 									<p class="row-in-form fill-wife">
 										<label class="checkbox-field">
